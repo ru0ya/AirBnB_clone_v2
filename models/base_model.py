3a483d1fff19d0ba53dcb1d8b714d1ba9f5a3fc9
Ahlam Abdulkhalek <alhamny@gmail.com>
2023-01-07 10:52:54 +0300

    Correction to deal with amenity

--- models/base_model.py
+++ models/base_model.py
@@ -19,9 +19,15 @@ class BaseModel:
         updated_at (sqlalchemy DateTime): The datetime of last update.
     """
 
-    id = Column(String(60), primary_key=True, nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    id = Column(String(60),
+                primary_key=True,
+                nullable=False)
+    created_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
+    updated_at = Column(DateTime,
+                        nullable=False,
+                        default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel.
