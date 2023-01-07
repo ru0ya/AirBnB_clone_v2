0d9633c7e81cd003106f49e72be9a270ec63e8f6
ru0ya <viktaangi02@gmail.com>
2023-01-07 22:48:23 +0300

    fixing

--- models/amenity.py
+++ models/amenity.py
@@ -3,32 +3,23 @@
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
-<<<<<<< HEAD
 from sqlalchemy import ForeignKey
-=======
->>>>>>> 0778974c353f927319c26f2301f32a2a98ee57c8
 from sqlalchemy import String
 from sqlalchemy.orm import relationship
-
-class Amenity(BaseModel, Base):
-    __tablename__ = "amenities"
-    if (storage_type == "db"):
-        name = Column(String(128), nullable=False)
-
-
-    else:
-        name = ""
 
 class Amenity(BaseModel, Base):
     """Represents an Amenity for a MySQL database.
     Inherits from SQLAlchemy Base and links to the MySQL table amenities.
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store Amenities.
-        name (sqlalchemy String): The amenity name.
-        place_amenities (sqlalchemy relationship): Place-Amenity relationship.
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary="place_amenity",
-                                   viewonly=False)
+    __tablename__ (str): The name of the MySQL table to store Amenities.
+    name (sqlalchemy String): The amenity name.
+    place_amenities (sqlalchemy relationship): Place-Amenity relationship.
+      """
 
+    __tablename__ = "amenities"
+    if (storage_type == "db"):
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity", viewonly=False)
+
+    else:
+        name = ""
--- models/review.py
+++ models/review.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     if (storage_type == "db"):
@@ -20,17 +20,3 @@ class Review(BaseModel):
         place_id = ""
         user_id = ""
         text = ""
-=======
-class Review(BaseModel, Base):
-    """Represents a review for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table reviews.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Reviews.
-        text (sqlalchemy String): The review description.
-        place_id (sqlalchemy String): The review's place id.
-        user_id (sqlalchemy String): The review's user id.
-    """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
