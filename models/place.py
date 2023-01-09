b999eeb126df9d074ee0cbbc245f664cae089c8d
ru0ya <viktaangi02@gmail.com>
2023-01-10 01:53:41 +0300

    work

--- models/place.py
+++ models/place.py
@@ -21,16 +21,16 @@ class Place(BaseModel):
     """ A place to stay """
     __tablename__ = "places"
     if(storage_type == "db"):
-        city_id = Column("city_id", String(60), nullable=False, ForeignKey('cities.id'))
-        user_id = Column("user_id", String(60), nullable=False, ForeignKey('users.id'))
-        name = Column("name", String(128), nullable=False)
-        description = Column("description", String(1024))
+        city_id = Column(String(60), nullable=False, ForeignKey('cities.id'))
+        user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True, default=0.0)
-        longitude = Column(Float, nullable=True, default=0.0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
         amenity_ids = []
         reviews = relationship("Review", back_populates="place")
         amenities = relationship("Amenity", secondary=place_amenity,
