5b13073d4a2056439880fce29420c0338446bc64
ru0ya <viktaangi02@gmail.com>
2023-02-09 10:18:20 +0300

    :runner:

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,6 +45,6 @@ def odd_or_even(n):
         n = int(n)
         return (render_template('6-number_odd_or_even.html', n=n))
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-
